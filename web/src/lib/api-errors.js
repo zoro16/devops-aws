5bbe50131938cbb49a3d78ed33640a8f61a62035
Mohamed Saleh <mohamed.saleh16@gmail.com>
2018-01-17 14:32:08 +0800

    remove all

--- web/src/lib/api-errors.js
+++
@@ -1,6 +0,0 @@
-// because Fetch doesn't recognize error responses as
-// actual errors since it's technically completing the response...
-export function handleApiErrors (response) {
-  if (!response.ok) throw Error(response.statusText)
-  return response
-}
